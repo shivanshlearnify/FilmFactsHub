4d78577d8f6d95fed83499ec438f56f00f3830fa
shivansh kesarwani <128142418+shivanshlearnify@users.noreply.github.com>
2023-12-20 11:25:04 +0530

    Created basic layout of Header and footer and made a connection with api

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import useFetch from "../src/hooks/useFetch";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+
+function App() {
+  
+
+  return (
+    <div>
+      <Header/>
+      
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
