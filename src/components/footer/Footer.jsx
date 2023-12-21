d61203ec1fd2c188b23c982b6dc1ad6f6f4eee56
shivansh kesarwani <128142418+shivanshlearnify@users.noreply.github.com>
2023-12-21 17:34:27 +0530

    Update on footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,7 +11,6 @@ const Footer = () => {
           <ul className="nav flex-column">
             <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
             <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
             <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
             <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
           </ul>
