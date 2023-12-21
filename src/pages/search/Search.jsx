8fa79c8a36e93afef149fdc9673733323d0888a2
shivansh kesarwani <128142418+shivanshlearnify@users.noreply.github.com>
2023-12-21 19:42:57 +0530

    updates on search item page

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+
+import "./style.css";
 
 const Search = () => {
-  return (
-    <div>Search</div>
-  )
-}
+  const { mediaType, id } = useParams();
 
-export default Search
+  const { data } = useFetch(`/${mediaType}/${id}`);
+  const { data: url } = useFetch("/configuration");
+  const baseUrl = url?.images?.secure_base_url + "original";
+  const searchImgUrl = baseUrl + data?.poster_path;
+  return (
+    <div>
+      <div className="detailsBanner">
+        <div className="searchPosterContainer">
+          <div className="searchPoster">
+            <img className="posterImg" src={searchImgUrl} alt="img" />
+          </div>
+        </div>
+        <div className="seacrhItemDeatils"></div>
+      </div>
+      <div className="searchCarousel"></div>
+    </div>
+  );
+};
+
+export default Search;
+
+
+
